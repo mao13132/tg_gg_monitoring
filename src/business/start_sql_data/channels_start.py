806f46b86a1bf54d94e28060c506b85a66fcbfcf
mao13132 <mao13132@gmail.com>
2024-10-23 17:05:39 +0300

    add channels and words

--- src/business/start_sql_data/channels_start.py
+++ src/business/start_sql_data/channels_start.py
@@ -7,6 +7,38 @@
 #
 # ---------------------------------------------
 channels_start_list = """
+https://t.me/crimpalsert
+https://t.me/yuristichat
+https://t.me/yurist_advokat_chat
+https://t.me/liga_yuristov
+https://t.me/chat_yurist
+https://t.me/chat_EU
+https://t.me/jurrussia
+https://t.me/juristhub
+https://t.me/jurist_onlain
+https://t.me/taran_bez_dolgov
+https://t.me/yuristrf1
+https://t.me/obedin_ip1
+https://t.me/propuskdonbass
+https://t.me/bankrotyurist
+https://t.me/Zibert_Svetlana
+https://t.me/birzaurist
+https://t.me/svovoenniiurist
+https://t.me/reg_regforum
+https://t.me/privlawlibchat
+https://t.me/zaschitauchitelei
+https://t.me/Pravoinformvoen
+https://t.me/prolgoti
+https://t.me/biletiz
+https://t.me/pravozel
+https://t.me/naradvokatchat
+https://t.me/corp_law_chat
+https://t.me/ryzhenkva_m
+https://t.me/doms_zabastovka
+https://t.me/advok24/1
+https://t.me/joinchat/xYHZEdKMLjMxM2Yy
+https://t.me/uristy_chat
+https://t.me/InterLawClub
 """
 
 
